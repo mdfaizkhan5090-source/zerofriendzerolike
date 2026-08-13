66daaf2c884817a9880999d5385b323764042375
mdfaizkhan5090-source <mdfaizkhan5090@gmail.com>
2026-08-13 16:07:22 +0530

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@ define('BOT_TOKEN', '8847236768:AAFCuxhfFt6rO4b_7y6_SRPyG2Xv2XyQki8');
 define('DB_PATH', __DIR__ . '/database.db');
 
 // ── REAL LIKE API ────────────────────────────────────────────────────────
-define('LIKE_API_URL', 'https://moderator456-like.vercel.app/like');
+define('LIKE_API_URL', 'https://free-fire-like-api-opal.vercel.app/like');
 
 // Optional key (leave empty)
 define('LIKE_API_KEY', '');
